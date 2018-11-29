45d440c746a2ccd21909e86e8811ff1f34c727a4
gry <275032718@qq.com>
2018-11-29 23:05:06 +0800

    up

--- test.py
+++ test.py
@@ -1,15 +1,43 @@
+# _*_ coding: utf8 _*_
+from bin.DyttList import dytt_list, dytt_detail
 from service.ImportService import ImportService
 
 import sys
-import requests
 import time
 import random
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-img = 'http://www.imageto.org/images/q96g.jpg'
+if __name__ == '__main__':
+    i = 5
+    total = 209
+    # total = 9
+    while i < total:
+        i = i + 1
+        # 日韩电影 url = 'http://www.ygdy8.net/html/gndy/rihan/index.html'
+        # 欧美电影 url = 'http://www.ygdy8.net/html/gndy/oumei/index.html'
+        # 国内电影 url = 'http://www.ygdy8.net/html/gndy/china/index.html'
+        # 综合电影 url = 'http://www.ygdy8.net/html/gndy/jddy/index.html'
+        # 最新电影 url = 'http://www.ygdy8.net/html/gndy/dyzz/index.html'
+        # url = 'http://www.ygdy8.net/html/gndy/oumei/list_7_%s.html' % (i, )
+        # url = 'http://www.ygdy8.net/html/gndy/china/list_4_%s.html' % (i, )
+        # url = 'http://www.ygdy8.net/html/gndy/rihan/list_6_%s.html' % (i, )
+        url = 'http://www.ygdy8.net/html/gndy/jddy/list_63_%s.html' % (i, )
+        # url = 'http://www.ygdy8.net/html/gndy/dyzz/list_23_%s.html' % (i, )
+        print url
 
-image = ImportService.upload_image(img, iscut=True, w=480, h=320)
-print '========================'
-print image
+        list = dytt_list(url)
+        tm = random.randint(10, 15)
+        time.sleep(tm)
+        # list = [{"link": "http://www.ygdy8.net/html/gndy/jddy/20140429/45014.html"}]
+
+        # 抓取页面数据
+        dytt_detail(url, list)
+        # if len(list) > 0:
+        #     for vo in list:
+        #         dytt_detail(url, [vo])
+        #         break
+
+        # 停止
+        # break
